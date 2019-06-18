3a5c524466de4da98456b394b6d00dd8ab550c17
Carlo James Lim <carlo@sprout.ph>
2019-06-18 19:39:38 +0800

    updates

---
+++ LambdaToSql.Tests/Entities/Employee.cs
@@ -0,0 +1,15 @@
+﻿using Dapper.Contrib.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LambdaToSql.Tests.Entities
+{
+    [Table("Employee")]
+    public class Employee
+    {
+        [Key]
+        public int EmployeeId { get; set; }
+        public string Fullname { get; set; }
+    }
+}
